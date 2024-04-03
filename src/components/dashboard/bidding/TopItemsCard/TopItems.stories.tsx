f4427f7e8d479add5c1e94cef60395e052947933
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2024-04-03 22:53:22 +0300

    chore: refactored stories imports and exports

--- src/components/dashboard/bidding/TopItemsCard/TopItems.stories.tsx
+++ src/components/dashboard/bidding/TopItemsCard/TopItems.stories.tsx
@@ -1,12 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import TopItemsData from '../../../../../public/mocks/BiddingTopSellers.json';
 
-import { TopItems } from './TopItems.tsx';
+import { TopItemsCard } from './TopItems.tsx';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'Components/Dashboard/Bidding/Top items table',
-  component: TopItems,
+  component: TopItemsCard,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
@@ -14,7 +14,7 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-} satisfies Meta<typeof TopItems>;
+} satisfies Meta<typeof TopItemsCard>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
