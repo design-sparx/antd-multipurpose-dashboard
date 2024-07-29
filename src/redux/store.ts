3523ab4e613c50e0a908b8db12fe0a02f9658161
ram-prasad23 <ramsteyn17@gmail.com>
2024-07-29 11:05:29 +0530

    fix:  netlify build fail due to typescript bug fixed

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,20 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import themeReducer from './theme/themeSlice';
+import themeReducer, { ThemeState } from './theme/themeSlice';
 import { persistReducer, persistStore, PersistConfig } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
+// Define the state shape
+interface RootState {
+  theme: ThemeState;
+}
+
+// Combine reducers
 const rootReducer = combineReducers({
   theme: themeReducer,
 });
 
-const persistConfig: PersistConfig<any> = {
+// Persist config with RootState
+const persistConfig: PersistConfig<RootState> = {
   key: 'root',
   storage,
   version: 1,
@@ -15,10 +22,17 @@ const persistConfig: PersistConfig<any> = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+// Configure store with persisted reducer
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }),
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
+// Persistor
 export const persistor = persistStore(store);
+
+// Type for RootState
+export type { RootState };
