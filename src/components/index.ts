c8861fcc381b8d94b08e3de5a8276480b754aa5d
Kelvin Kiprop <kelvin.kiprop96@gmail.com>
2025-12-18 00:56:33 +0300

    chore: remove DataModeToggle component and related exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ export { BackBtn } from './BackBtn/BackBtn.tsx';
 export { BlogsListCard } from './corporate/BlogsListCard/BlogsListCard';
 export { Card } from './Card/Card';
 export { ContactForm } from './ContactForm/ContactForm';
-export { DataModeToggle } from './DataModeToggle/DataModeToggle';
 export { EmployeeCard } from './EmployeeCard/EmployeeCard';
 export { FaqCollapse } from './FaqCollapse/FaqCollapse';
 export { Flex } from './Flex/Flex';
