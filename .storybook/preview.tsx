736f29ed4cd9128f9465e5324d5e07bb39c34d2d
Kelvin Kiprop <kelvin.kiprop96@gmail.com>
2026-02-12 23:52:35 +0300

    feat: configure Redux store in Storybook preview for isolated component testing

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,13 +1,24 @@
 import type { Preview } from '@storybook/react';
 import { themes } from '@storybook/theming';
+import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-import { store } from '../src/redux/store';
+import themeReducer from '../src/redux/theme/themeSlice';
+import dataModeReducer from '../src/redux/data-mode/dataModeSlice';
+import authReducer from '../src/redux/auth/authSlice';
 import { StylesContext } from '../src/context';
 import '../src/App.css';
 
+const storybookStore = configureStore({
+  reducer: {
+    theme: themeReducer,
+    dataMode: dataModeReducer,
+    auth: authReducer,
+  },
+});
+
 export const withStylesProvider = (Story: any) => {
   return (
-    <Provider store={store}>
+    <Provider store={storybookStore}>
       <StylesContext.Provider value={null}>
         <Story />
       </StylesContext.Provider>
